e86d7f5c9925090194d127bba14c900bd41bf15a
Lareta064 <lareta064@gmail.com>
2024-10-09 18:10:48 +0300

    adapt-mob-menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,23 +4,27 @@ document.addEventListener("DOMContentLoaded", function () {
 	const openMenuLevel2 = document.querySelectorAll('.drop-menu_2');
 	const openMenuLevel3 = document.querySelectorAll('.drop-menu_3');
 	const openMenuLevel4 = document.querySelectorAll('.drop-menu_4');
-	
-
-
 
 	function foldWithChildren(dropMenuItem) {
 		let itemIcon = dropMenuItem.querySelector(".drop-icon");
 		let childrenMenu = dropMenuItem.querySelector(".submenu");
 
+		// Проверяем, существует ли элемент childrenMenu
+		if (!childrenMenu) {
+			return; // Если подменю нет, выходим из функции
+		}
+
 		itemIcon.classList.remove('active');
 		childrenMenu.classList.remove('active');
 
+		// Также проверяем, есть ли вложенные элементы перед тем, как пытаться их сворачивать
 		let childrenMenuChildren = childrenMenu.querySelectorAll('.drop-menu');
-		for (let item of childrenMenuChildren) {
-			foldWithChildren(item);
+		if (childrenMenuChildren.length > 0) {
+			for (let item of childrenMenuChildren) {
+				foldWithChildren(item);
+			}
 		}
 	}
-
 	function goUpAndFoldSiblings(dropMenuItem) {
 		let ancestor = dropMenuItem.parentElement.closest('.drop-menu');
 
@@ -46,8 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	function showSubmenu(item, subMenuClass) {
 		item.addEventListener('click', function (e) {
-			//e.stopPropagation();
-
+			
 			const thisIcon = this.querySelector('.drop-icon')
 			const subMenuLevel = this.querySelector(`${subMenuClass}`)
 
@@ -55,8 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 				if (thisIcon.classList.contains('active')) {
 					foldWithChildren(item);
-					//subMenuLevel.classList.remove('active');
-					//thisIcon.classList.remove('active');
+					
 				} else {
 					goUpAndFoldSiblings(item);
 					subMenuLevel.classList.add('active');
@@ -75,32 +77,5 @@ document.addEventListener("DOMContentLoaded", function () {
 		showSubmenu(item, '.submenu-4');
 	}
 	
-	// const lastDropMenuLevel3 = document.querySelector(".submenu--last .submenu-3");
-	// const lastDropMenuLevel4 = document.querySelector(".submenu--last .submenu-4");
-
-	// const mediumDropMenuLevel3 = document.querySelector(".submenu--medium .submenu-3");
-	// const mediumDropMenuLevel4 = document.querySelector(".submenu--medium .submenu-4");
-
-	// if (window.innerWidth > 991 && window.innerWidth < 1440) {
-	// 	if (lastDropMenuLevel3) {
-	// 		lastDropMenuLevel3.classList.add('open-left')
-	// 	}
-
-	// 	if (lastDropMenuLevel4) {
-	// 		lastDropMenuLevel4.classList.remove('open-left')
-	// 	}
-	// 	if (mediumDropMenuLevel4) {
-	// 		lastDropMenuLevel4.classList.add('open-left')
-	// 	}
-	// }
-	// if (window.innerWidth > 1900) {
-	// 	if (lastDropMenuLevel3) {
-	// 		lastDropMenuLevel3.classList.remove('open-left')
-	// 	}
-	// 	if (lastDropMenuLevel4) {
-	// 		lastDropMenuLevel4.classList.add('open-left')
-	// 	}
-
-	// }
-
+	
 });
